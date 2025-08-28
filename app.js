07f8ead2157a756519a361112e3d6fe42b1d334b
Ethan Watts <ethan.watts@live.co.uk>
2025-08-28 11:23:20 +0000

    add user auth system

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const flash = require("connect-flash");
 const port = 3000;
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const { exposeFlashMsgs } = require("./middleware/flash");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -15,18 +19,29 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Session and flash middleware setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(exposeFlashMsgs);
 
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- models/model.js
+++ models/model.js
@@ -1,134 +1,139 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-const seq = new Sequelize('portfolio_manager', process.env.DB_USER, process.env.DB_PASSWORD, {
+const seq = new Sequelize(
+  "portfolio_manager",
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: "mysql"
-});
+    dialect: "mysql",
+  }
+);
 
-const User = seq.define('User', {
+const User = seq.define(
+  "User",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     email: {
-        type: DataTypes.STRING(255),
-        unique: true,
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      unique: true,
+      allowNull: false,
     },
     hashed_pass: {
-        type: DataTypes.CHAR(32),
-        allowNull: false,
+      type: DataTypes.CHAR(32),
+      allowNull: false,
     },
-}, {
-    tableName: 'users',
-    schema: 'portfolio_manager',
+  },
+  {
+    tableName: "users",
     timestamps: false,
-});
+  }
+);
 
-
-const Asset = seq.define('Asset', {
+const Asset = seq.define(
+  "Asset",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     ticker: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     name: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     category: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     quantity: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
     },
     buy_price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
     },
     current_price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
     },
     currency: {
-        type: DataTypes.STRING(3),
-        allowNull: false,
+      type: DataTypes.STRING(3),
+      allowNull: false,
     },
     purchased_at: {
-        type: DataTypes.DATE,
-        defaultValue: seq.literal('CURRENT_TIMESTAMP'),
-
+      type: DataTypes.DATE,
+      defaultValue: seq.literal("CURRENT_TIMESTAMP"),
     },
     type: {
-        type: DataTypes.ENUM('buy', 'sell', 'keep'),
-        allowNull: false,
+      type: DataTypes.ENUM("buy", "sell", "keep"),
+      allowNull: false,
     },
-}, {
-    tableName: 'assets',
+  },
+  {
+    tableName: "assets",
     timestamps: false,
-});
-
-
-
-
+  }
+);
 
 //current iteration, functionality to be changed
 
-
 //user can get all assets in their portfolio
 async function getAssetsByUserId(userId) {
-    return await Asset.findAll({
-        where: {
-            user_id: userId
-        }
-    });
+  return await Asset.findAll({
+    where: {
+      user_id: userId,
+    },
+  });
 }
 
 //user can search by category
 async function getAssetsByCategory(category) {
-    return await Asset.findAll({
-        where: {
-            category: category
-        }
-    });
+  return await Asset.findAll({
+    where: {
+      category: category,
+    },
+  });
 }
 //user can get assets by ticker, to get a specific asset
 async function getAssetsByTicker(ticker) {
-    return await Asset.findAll({
-        where: {
-            ticker: ticker
-        }
-    });
+  return await Asset.findAll({
+    where: {
+      ticker: ticker,
+    },
+  });
 }
 //user can see if assets are ones bought, sold or kept
 async function getAssetsByType(type) {
-    return await Asset.findAll({
-        where: {
-            type: type
-        }
-    });
+  return await Asset.findAll({
+    where: {
+      type: type,
+    },
+  });
 }
 
 module.exports = {
-    seq,
-    User,
-    Asset,
-    getAssetsByUserId,
-    getAssetsByCategory,
-    getAssetsByTicker,
-    getAssetsByType
+  seq,
+  User,
+  Asset,
+  getAssetsByUserId,
+  getAssetsByCategory,
+  getAssetsByTicker,
+  getAssetsByType,
 };
