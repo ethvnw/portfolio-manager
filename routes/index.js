8d3ae1a738ffcec92000ef5f2df4f6aa8f3fee90
Hassan Iqbal <hassan.iqba@hsbc.com>
2025-08-28 10:48:39 +0000

    Busy and sell assets pages and navigation links

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { const1: 'value' });
 });
 
+router.get('/buyassets', function(req, res, next) {
+  res.render('buyassets.pug', { const1: 'value' });
+});
 
-
+router.get('/sellassets', function(req, res, next) {
+  res.render('sellassets.pug', { const1: 'value' });
+});
 
 module.exports = router;
