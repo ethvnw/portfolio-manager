dac7e0ce68584f3bf068041ac0a066361c8c5b5f
Ethan Watts <ethan.watts@live.co.uk>
2025-08-29 08:48:44 +0000

    add sell asset functionality

--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller");
+const assetController = require("../controller/asset_controller");
 const { ensureAuthenticated } = require("../middleware/auth");
-const { getAssetsByUserId, Asset } = require("../models/model");
-const axios = require("axios");
-require("dotenv").config();
 
 router.get("/", ensureAuthenticated, (req, res, next) => {
   res.render("index");
 });
 
-router.get("/my-portfolio", ensureAuthenticated, async (req, res, next) => {
-  const portfolioItems = await getAssetsByUserId(req.user.id);
-  res.render("my_portfolio", { portfolioItems });
-});
-
-const getCurrentAssetPrice = async (symbol) => {
-  const apiUrl = `${process.env.API_URL}${symbol}`;
-  try {
-    const response = await axios.get(apiUrl);
-    const data = response.data;
-    return data["Time Series (Daily)"][
-      Object.keys(data["Time Series (Daily)"])[0]
-    ]["4. close"];
-  } catch (error) {
-    console.error("Error fetching asset current price:", error);
-    throw error;
-  }
-};
+router.get("/my-portfolio", ensureAuthenticated, assetController.portfolio);
 
 router.get(
   "/asset-current-price",
   ensureAuthenticated,
-  async (req, res, next) => {
-    const { symbol } = req.query;
-    try {
-      const currentPrice = await getCurrentAssetPrice(symbol);
-      res.json({ price: currentPrice });
-    } catch (error) {
-      console.error("Error fetching asset current price:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
+  assetController.assetCurrentPrice
 );
-
-router.get("/buy-asset", ensureAuthenticated, function (req, res, next) {
-  res.render("buy_asset", { const1: "value" });
-});
-
-router.post("/buy-asset", ensureAuthenticated, async (req, res, next) => {
-  const { assetName, assetTicker, assetType, quantity, buyPrice } = req.body;
-  try {
-    const newAsset = await Asset.create({
-      user_id: req.user.id,
-      name: assetName,
-      ticker: assetTicker ? assetTicker : "N/A",
-      category: assetType,
-      quantity: parseFloat(quantity),
-      buy_price: parseFloat(buyPrice),
-      current_price: assetTicker ? await getCurrentAssetPrice(assetTicker) : 0,
-      currency: "USD",
-      type: "buy",
-    });
-    req.flash("notice", "Asset purchased successfully!");
-    res.redirect("/my-portfolio");
-  } catch (error) {
-    console.error("Error purchasing asset:", error);
-    req.flash("error", "Error purchasing asset. Please try again.");
-    res.redirect("/buy-asset");
-  }
-});
-
-router.get("/sell-asset", ensureAuthenticated, function (req, res, next) {
-  res.render("sell_asset", { const1: "value" });
-});
+router.get("/buy-asset", ensureAuthenticated, assetController.renderBuyAsset);
+router.post("/buy-asset", ensureAuthenticated, assetController.buyAsset);
+router.get("/sell-asset", ensureAuthenticated, assetController.renderSellAsset);
+router.post("/sell-asset", ensureAuthenticated, assetController.sellAsset);
 
 // restful API routes
 // router.get("/users/:email", controller.getUserByEmail);
